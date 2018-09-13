eed85490d828f1a85fccd5ba2c97f4a26eff6e78
Wenjie Wei <wenjieweizb@gmail.com>
2018-09-13 18:40:54 -0400

    C code complete. Working on assembly.

---
+++ GXX_Lab0/asm/filter_s.s
@@ -0,0 +1,18 @@
+	AREA text, CODE, READONLY
+	export filter_s
+filter_s
+	; This is the subroutine for calculating the filtered value.
+	; There are four input arguments:
+	; R1 for the location of array OriginalSignal
+	; R2 for the location of array FilteredSignal
+	; R3 for the number of N
+	; R4 for the number of D
+	
+	AND R7, R4, #1		; R7 stores if D is odd. 1 for odd, 0 for even.
+	
+	MOVS R5, #0x00		; i is the counter of the outer loop. Stored in R5.
+	CMP R5, 	
+
+	BX LR
+	
+	END
